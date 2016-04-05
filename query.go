3b33091d435d159e0abb7ce39845c30ff60e7aba
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2016-04-05 12:39:00 +0300

    Don't escape QueryAppender.

--- query.go
+++ query.go
@@ -27,15 +27,15 @@ func (q shardQuery) AppendQuery(dst []byte, params []interface{}) ([]byte, error
 	switch query := q.query.(type) {
 	case string:
 		b = append(b[:0], query...)
+		return q.fmter.AppendBytes(dst, b, params, true), nil
 	case orm.QueryAppender:
 		var err error
 		b, err = query.AppendQuery(b[:0], nil)
 		if err != nil {
 			return nil, err
 		}
+		return q.fmter.AppendBytes(dst, b, params, false), nil
 	default:
 		return nil, fmt.Errorf("unsupported query type: %T", query)
 	}
-
-	return q.fmter.AppendBytes(dst, b, params, false), nil
 }
