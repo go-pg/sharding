b54fef47669212131686f26b99414070ebf20abc
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2018-04-13 13:26:02 +0300

    Add SubCluster

--- cluster.go
+++ cluster.go
@@ -64,13 +64,6 @@ func (cl *Cluster) init() {
 	}
 }
 
-// SplitShard uses SplitId to extract shard id from the id and then
-// returns corresponding cluster Shard.
-func (cl *Cluster) SplitShard(id int64) *pg.DB {
-	_, shardId, _ := SplitId(id)
-	return cl.Shard(shardId)
-}
-
 func (cl *Cluster) newShard(db *pg.DB, id int64) *pg.DB {
 	name := "shard" + strconv.FormatInt(id, 10)
 	return db.WithParam("shard_id", id).
@@ -115,10 +108,17 @@ func (cl *Cluster) Shards(db *pg.DB) []*pg.DB {
 	return shards
 }
 
-// Shard maps the number to the corresponding shard.
+// Shard maps the number to the corresponding shard in the cluster.
 func (cl *Cluster) Shard(number int64) *pg.DB {
 	number = number % int64(len(cl.shards))
 	return cl.shards[number]
+}
+
+// SplitShard uses SplitId to extract shard id from the id and then
+// returns corresponding Shard in the cluster.
+func (cl *Cluster) SplitShard(id int64) *pg.DB {
+	_, shardId, _ := cl.gen.SplitId(id)
+	return cl.Shard(shardId)
 }
 
 // ForEachDB concurrently calls the fn on each database in the cluster.
@@ -149,15 +149,15 @@ func (cl *Cluster) ForEachDB(fn func(db *pg.DB) error) error {
 
 // ForEachShard concurrently calls the fn on each shard in the cluster.
 // It is the same as ForEachNShards(1, fn).
-func (cl *Cluster) ForEachShard(fn func(shardId int, shard *pg.DB) error) error {
+func (cl *Cluster) ForEachShard(fn func(shard *pg.DB) error) error {
 	return cl.ForEachDB(func(db *pg.DB) error {
 		var firstErr error
-		for i, shard := range cl.shards {
-			if cl.dbs[i%len(cl.dbs)] != db {
+		for _, shard := range cl.shards {
+			if shard.Options() != db.Options() {
 				continue
 			}
 
-			if err := fn(i, shard); err != nil && firstErr == nil {
+			if err := fn(shard); err != nil && firstErr == nil {
 				firstErr = err
 			}
 		}
@@ -166,31 +166,122 @@ func (cl *Cluster) ForEachShard(fn func(shardId int, shard *pg.DB) error) error 
 }
 
 // ForEachNShards concurrently calls the fn on each N shards in the cluster.
-func (cl *Cluster) ForEachNShards(n int, fn func(shardId int, shard *pg.DB) error) error {
+func (cl *Cluster) ForEachNShards(n int, fn func(shard *pg.DB) error) error {
 	return cl.ForEachDB(func(db *pg.DB) error {
 		var wg sync.WaitGroup
 		errCh := make(chan error, 1)
 		limit := make(chan struct{}, n)
 
-		for i, shard := range cl.shards {
-			if cl.dbs[i%len(cl.dbs)] != db {
+		for _, shard := range cl.shards {
+			if shard.Options() != db.Options() {
 				continue
 			}
 
 			limit <- struct{}{}
 			wg.Add(1)
-			go func(i int, shard *pg.DB) {
+			go func(shard *pg.DB) {
 				defer func() {
 					<-limit
 					wg.Done()
 				}()
-				if err := fn(i, shard); err != nil {
+				if err := fn(shard); err != nil {
 					select {
 					case errCh <- err:
 					default:
 					}
 				}
-			}(i, shard)
+			}(shard)
+		}
+
+		wg.Wait()
+
+		select {
+		case err := <-errCh:
+			return err
+		default:
+			return nil
+		}
+	})
+}
+
+// SubCluster is a subset of the cluster.
+type SubCluster struct {
+	cl     *Cluster
+	shards []*pg.DB
+}
+
+// SubCluster returns a subset of the cluster of the given size.
+func (cl *Cluster) SubCluster(number int64, size int) *SubCluster {
+	step := len(cl.shards) / size
+	clusterId := int(number%int64(step)) * size
+	shards := make([]*pg.DB, 0, size)
+	for i := 0; i < size; i++ {
+		shards = append(shards, cl.shards[clusterId+i])
+	}
+
+	return &SubCluster{
+		cl:     cl,
+		shards: shards,
+	}
+}
+
+// SplitShard uses SplitId to extract shard id from the id and then
+// returns corresponding Shard in the subcluster.
+func (cl *SubCluster) SplitShard(id int64) *pg.DB {
+	_, shardId, _ := cl.cl.gen.SplitId(id)
+	return cl.Shard(shardId)
+}
+
+// Shard maps the number to the corresponding shard in the subscluster.
+func (cl *SubCluster) Shard(number int64) *pg.DB {
+	number = number % int64(len(cl.shards))
+	return cl.shards[number]
+}
+
+// ForEachShard concurrently calls the fn on each shard in the subcluster.
+// It is the same as ForEachNShards(1, fn).
+func (cl *SubCluster) ForEachShard(fn func(shard *pg.DB) error) error {
+	return cl.cl.ForEachDB(func(db *pg.DB) error {
+		var firstErr error
+		for _, shard := range cl.shards {
+			if shard.Options() != db.Options() {
+				continue
+			}
+
+			if err := fn(shard); err != nil && firstErr == nil {
+				firstErr = err
+			}
+		}
+		return firstErr
+	})
+}
+
+// ForEachNShards concurrently calls the fn on each N shards in the subcluster.
+func (cl *SubCluster) ForEachNShards(n int, fn func(shard *pg.DB) error) error {
+	return cl.cl.ForEachDB(func(db *pg.DB) error {
+		var wg sync.WaitGroup
+		errCh := make(chan error, 1)
+		limit := make(chan struct{}, n)
+
+		for _, shard := range cl.shards {
+			if shard.Options() != db.Options() {
+				continue
+			}
+
+			limit <- struct{}{}
+			wg.Add(1)
+			go func(shard *pg.DB) {
+				defer func() {
+					<-limit
+					wg.Done()
+				}()
+				if err := fn(shard); err != nil {
+					select {
+					case errCh <- err:
+					default:
+					}
+				}
+			}(shard)
 		}
 
 		wg.Wait()
