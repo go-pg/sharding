8d99a3e027c6b6c9443a6ab41c0c6340f62f2c03
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-07-25 14:11:27 +0300

    Be strict and don't auto-fix user config

--- cluster.go
+++ cluster.go
@@ -45,15 +45,8 @@ func NewClusterWithGen(dbs []*pg.DB, nshards int, gen *IdGen) *Cluster {
 	if nshards < len(dbs) {
 		panic("number of shards must be greater or equal number of dbs")
 	}
-
-	// Fill up holes using dbs from the first half of the cluster.
-	// E.g. [db1, db2, db3] becomes [db1, db2, db3, db2].
-	ndbs := len(dbs)
-	for nshards%ndbs != 0 {
-		ndbs++
-	}
-	for i := len(dbs); i < ndbs; i++ {
-		dbs = append(dbs, dbs[i-ndbs/2])
+	if nshards%len(dbs) != 0 {
+		panic("number of shards must be divideable by number of dbs")
 	}
 
 	cl := &Cluster{
